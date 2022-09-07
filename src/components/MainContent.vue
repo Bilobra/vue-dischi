d03157c270acb60ef0f92bc3f5dc894110634e27
Bilobra <bra.carrozzo@gmail.com>
2022-09-07 16:19:48 +0200

    add style

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,14 +1,14 @@
 <template>
     <main class="py-5">
-        <div class="container mx-auto">
-            <div class="row">
-                <div class="col" v-for="(disk,i) in listDisk" :key="i">
-                    <div class="card" style="width: 18rem;">
+        <div class="container">
+            <div class="row row-cols-lg-5 gy-5">
+                <div class="col py-4" v-for="(disk,i) in listDisk" :key="i">
+                    <div class="card align-items-center gap-3 py-3 text-center" style="width: 16rem; height: 25rem;">
                         <img :src="disk.poster" class="card-img-top" :alt="disk.title">
                         <div class="card-body">
-                            <h5 class="card-title">{{disk.title}}</h5>
-                            <p class="card-text">{{disk.author}}</p>
-                            <p class="card-text">{{disk.year}}</p>
+                            <h5 class="card-title title">{{disk.title}}</h5>
+                            <p class="card-text author">{{disk.author}}</p>
+                            <p class="card-text year">{{disk.year}}</p>
 
 
                         </div>
@@ -32,7 +32,7 @@ export default {
             .get('https://flynn.boolean.careers/exercises/api/array/music')
             .then((res) => {
                 console.log(res.data)
-                this.listDisk = res.data;
+                this.listDisk = res.data.response;
             })
     }
 
@@ -47,5 +47,31 @@ export default {
 main {
     height: 100vh;
     background-color: $--dark-color;
+}
+
+.card {
+    background-color: $--light-color;
+
+    .title {
+        color: $--font-color;
+        text-transform: uppercase;
+    }
+
+    .author,
+    .year {
+        font-size: 20px;
+        color: #757673;
+    }
+
+    img {
+        opacity: 0.7;
+
+        &:hover {
+            scale: 1.1;
+            border-radius: 5px;
+            border: 2px solid $--font-color;
+            opacity: 1;
+        }
+    }
 }
 </style>
