50d61e56338d1928af08ce2d15a465122a06b375
Bilobra <bra.carrozzo@gmail.com>
2022-09-07 15:17:09 +0200

    add axios + v-for

---
+++ src/components/MainContent.vue
@@ -0,0 +1,51 @@
+<template>
+    <main class="py-5">
+        <div class="container mx-auto">
+            <div class="row">
+                <div class="col" v-for="(disk,i) in listDisk" :key="i">
+                    <div class="card" style="width: 18rem;">
+                        <img :src="disk.poster" class="card-img-top" :alt="disk.title">
+                        <div class="card-body">
+                            <h5 class="card-title">{{disk.title}}</h5>
+                            <p class="card-text">{{disk.author}}</p>
+                            <p class="card-text">{{disk.year}}</p>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            listDisk: [],
+        }
+    },
+    created() {
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((res) => {
+                console.log(res.data)
+                this.listDisk = res.data;
+            })
+    }
+
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/var.scss';
+@import '../styles/general.scss';
+
+main {
+    height: 100vh;
+    background-color: $--dark-color;
+}
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <header>
+
+        <div class="row">
+            <div class="col d-flex ps-5">
+
+                <img src="../assets/logo-small.svg" alt="logo spotify">
+
+
+            </div>
+
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/var.scss';
+
+header {
+    background-color: $--light-color;
+
+    .row {
+        height: 80px;
+
+        .col {
+            align-items: center;
+
+            img {
+                max-width: 50px;
+                height: 50px;
+                
+            }
+        }
+
+    }
+}
+</style>
