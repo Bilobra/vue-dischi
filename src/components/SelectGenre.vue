f92d24d79774884453f5ca8911ccaeff250f03c9
Bilobra <bra.carrozzo@gmail.com>
2022-09-08 14:54:17 +0200

    push to hotwell

--- src/components/SelectGenre.vue
+++ src/components/SelectGenre.vue
@@ -1,15 +1,24 @@
 <template>
     <div>
-        <select name="genre" id="">
+        <select name="genre" id="" v-model="chooseGenre">
             <option value="all">all</option>
-            <option></option>
+            <option value="pop">Pop</option>
+            <option value="rock">Rock</option>
+            <option value="jazz">Jazz</option>
+            <option value="metal">Metal</option>
         </select>
     </div>
 </template>
 
 <script>
     export default{
+        name: 'SelectGenre',
 
+        data(){
+            return{
+                chooseGenre : "",
+            }
+        }
     }
 </script>
 
