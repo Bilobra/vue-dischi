8a23d0fe01306659136cc6b05747da99f96d5345
Bilobra <bra.carrozzo@gmail.com>
2022-09-08 13:56:41 +0200

    add input in Header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,11 +1,14 @@
 <template>
-    <header class="d-flex">
+    <header class="d-flex gap-5">
 
 
         <figure class="d-flex align-items-center m-0 ps-2">
             <img src="../assets/logo-small.svg" alt="logo spotify">
 
         </figure>
+        <div class="d-flex align-items-center">
+            <input type="text" placeholder="cerca...">
+        </div>
 
 
 
@@ -26,11 +29,21 @@ export default {
 header {
     background-color: $--light-color;
     height: 80px;
+    padding: 0 20px;
 
     img {
         max-width: 50px;
         height: 50px;
 
+    }
+
+    input{
+        padding: 5px 8px;
+        line-height: 20px;
+        border: none;
+        border-radius: 3px;
+        background-color: $--dark-color;
+        color:white;
     }
 
 
